91b535df2d6251ea79ceca794f73f1b980c62ad3
Federico González <federico1818@gmail.com>
2024-12-14 11:04:26 -0300

    create nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { NavComponent } from './components/nav/nav.component'
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    imports: [
+        RouterOutlet,
+        NavComponent
+    ]
 })
 
 export class AppComponent {
